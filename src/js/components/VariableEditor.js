f4e06fd7172e922adc686e1ead939da2b337cb2b
Mike Seese <seesemichaelj@gmail.com>
2018-09-27 18:12:50 -0400

    remove quotation marks around keys

--- src/js/components/VariableEditor.js
+++ src/js/components/VariableEditor.js
@@ -81,11 +81,9 @@ class VariableEditor extends React.PureComponent {
                             class="object-key"
                             key={variable.name + '_' + namespace}
                         >
-                            <span style={{ verticalAlign: 'top' }}>"</span>
                             <span style={{ display: 'inline-block' }}>
                                 {variable.name}
                             </span>
-                            <span style={{ verticalAlign: 'top' }}>"</span>
                         </span>
                         <span {...Theme(theme, 'colon')}>:</span>
                     </span>
